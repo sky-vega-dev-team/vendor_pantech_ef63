453c3a6f615e65df558bfc2809a7aefb681a05fc
hiru <rlawoehd187@naver.com>
2020-01-13 16:46:26 +0900

    ef51: fix build error

--- ef51/ef51-vendor-blobs.mk
+++ ef51/ef51-vendor-blobs.mk
@@ -46,7 +46,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     vendor/pantech/ef51/proprietary/etc/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
-    vendor/pantech/ef51/proprietary/etc/firmwarea225_pm4.fw:system/etc/firmware/a225_pm4.fw \
+    vendor/pantech/ef51/proprietary/etc/firmware/a225_pm4.fw:system/etc/firmware/a225_pm4.fw \
     vendor/pantech/ef51/proprietary/etc/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
     vendor/pantech/ef51/proprietary/etc/firmware/cyttsp_8064_mtp.hex:system/etc/firmware/cyttsp_8064_mtp.hex \
     vendor/pantech/ef51/proprietary/etc/firmware/cyttsp_8960_cdp.hex:system/etc/firmware/cyttsp_8960_cdp.hex \
