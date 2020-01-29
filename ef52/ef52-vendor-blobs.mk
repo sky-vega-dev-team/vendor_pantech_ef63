a77b1473f110a1cafa82759bab83ec85398b6cb8
hiru <rlawoehd187@naver.com>
2020-01-29 10:44:16 +0900

    ef52: move to vendor for O

--- ef52/ef52-vendor-blobs.mk
+++ ef52/ef52-vendor-blobs.mk
@@ -16,52 +16,52 @@
 
 #IM-A870L
 PRODUCT_COPY_FILES += \
-    vendor/pantech/ef52/proprietary/etc/firmware_ef52l/vidcfw.elf:system/etc/firmware_ef52l/vidcfw.elf \
-    vendor/pantech/ef52/proprietary/etc/firmware_ef52l/vidc.mdt:system/etc/firmware_ef52l/vidc.mdt \
-    vendor/pantech/ef52/proprietary/etc/firmware_ef52l/vidc.b00:system/etc/firmware_ef52l/vidc.b00 \
-    vendor/pantech/ef52/proprietary/etc/firmware_ef52l/vidc.b01:system/etc/firmware_ef52l/vidc.b01 
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware_ef52l/vidcfw.elf:$(TARGET_COPY_OUT_VENDOR)/etc/firmware_ef52l/vidcfw.elf \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware_ef52l/vidc.mdt:$(TARGET_COPY_OUT_VENDOR)/etc/firmware_ef52l/vidc.mdt \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware_ef52l/vidc.b00:$(TARGET_COPY_OUT_VENDOR)/etc/firmware_ef52l/vidc.b00 \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware_ef52l/vidc.b01:$(TARGET_COPY_OUT_VENDOR)/etc/firmware_ef52l/vidc.b01 
 
 #IM-A870S
 PRODUCT_COPY_FILES += \
-    vendor/pantech/ef52/proprietary/etc/firmware_ef52s/vidcfw.elf:system/etc/firmware_ef52s/vidcfw.elf \
-    vendor/pantech/ef52/proprietary/etc/firmware_ef52s/vidc.mdt:system/etc/firmware_ef52s/vidc.mdt \
-    vendor/pantech/ef52/proprietary/etc/firmware_ef52s/vidc.b00:system/etc/firmware_ef52s/vidc.b00 \
-    vendor/pantech/ef52/proprietary/etc/firmware_ef52s/vidc.b01:system/etc/firmware_ef52s/vidc.b01 
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware_ef52s/vidcfw.elf:$(TARGET_COPY_OUT_VENDOR)/etc/firmware_ef52s/vidcfw.elf \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware_ef52s/vidc.mdt:$(TARGET_COPY_OUT_VENDOR)/etc/firmware_ef52s/vidc.mdt \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware_ef52s/vidc.b00:$(TARGET_COPY_OUT_VENDOR)/etc/firmware_ef52s/vidc.b00 \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware_ef52s/vidc.b01:$(TARGET_COPY_OUT_VENDOR)/etc/firmware_ef52s/vidc.b01 
 
 #IM-A870K
 PRODUCT_COPY_FILES += \
-    vendor/pantech/ef52/proprietary/etc/firmware_ef52k/vidcfw.elf:system/etc/firmware_ef52k/vidcfw.elf \
-    vendor/pantech/ef52/proprietary/etc/firmware_ef52k/vidc.mdt:system/etc/firmware_ef52k/vidc.mdt \
-    vendor/pantech/ef52/proprietary/etc/firmware_ef52k/vidc.b00:system/etc/firmware_ef52k/vidc.b00 \
-    vendor/pantech/ef52/proprietary/etc/firmware_ef52k/vidc.b01:system/etc/firmware_ef52k/vidc.b01 
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware_ef52k/vidcfw.elf:$(TARGET_COPY_OUT_VENDOR)/etc/firmware_ef52k/vidcfw.elf \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware_ef52k/vidc.mdt:$(TARGET_COPY_OUT_VENDOR)/etc/firmware_ef52k/vidc.mdt \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware_ef52k/vidc.b00:$(TARGET_COPY_OUT_VENDOR)/etc/firmware_ef52k/vidc.b00 \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware_ef52k/vidc.b01:$(TARGET_COPY_OUT_VENDOR)/etc/firmware_ef52k/vidc.b01 
 
 PRODUCT_COPY_FILES += \
-    vendor/pantech/ef52/proprietary/bin/sensors.qcom:system/bin/sensors.qcom \
-    vendor/pantech/ef52/proprietary/lib/libmmcamera_faceproc.so:system/lib/libmmcamera_faceproc.so \
-    vendor/pantech/ef52/proprietary/lib/libmmcamera_frameproc.so:system/lib/libmmcamera_frameproc.so \
-    vendor/pantech/ef52/proprietary/lib/libmmcamera_hdr_lib.so:system/lib/libmmcamera_hdr_lib.so \
-    vendor/pantech/ef52/proprietary/lib/libmmcamera_image_stab.so:system/lib/libmmcamera_image_stab.so \
-    vendor/pantech/ef52/proprietary/lib/libmmcamera_interface.so:system/lib/libmmcamera_interface.so \
-    vendor/pantech/ef52/proprietary/lib/libmmcamera_interface2.so:system/lib/libmmcamera_interface2.so \
-    vendor/pantech/ef52/proprietary/lib/libmmcamera_plugin.so:system/lib/libmmcamera_plugin.so \
-    vendor/pantech/ef52/proprietary/lib/libmmcamera_statsproc31.so:system/lib/libmmcamera_statsproc31.so \
-    vendor/pantech/ef52/proprietary/lib/libmmcamera_wavelet_lib.so:system/lib/libmmcamera_wavelet_lib.so \
-    vendor/pantech/ef52/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
-    vendor/pantech/ef52/proprietary/lib/libsensor1.so:system/lib/libsensor1.so \
-    vendor/pantech/ef52/proprietary/lib/libsensor_reg.so:system/lib/libsensor_reg.so \
-    vendor/pantech/ef52/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
-    vendor/pantech/ef52/proprietary/lib/hw/camera.msm8960.so:system/lib/hw/camera.msm8960.so \
-    vendor/pantech/ef52/proprietary/lib/hw/sensors.msm8960.so:system/lib/hw/sensors.msm8960.so \
-    vendor/pantech/ef52/proprietary/lib/hw/sensors_native_hal.so:system/lib/hw/sensors_native_hal.so \
-    vendor/pantech/ef52/proprietary/lib/hw/sensors_qcom.so:system/lib/hw/sensors_qcom.so \
-    vendor/pantech/ef52/proprietary/lib/hw/lights.msm8960.so:system/lib/hw/lights.msm8960.so \
+    vendor/pantech/ef52/proprietary/vendor/bin/sensors.qcom:$(TARGET_COPY_OUT_VENDOR)/bin/sensors.qcom \
+    vendor/pantech/ef52/proprietary/vendor/lib/libmmcamera_faceproc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_faceproc.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/libmmcamera_frameproc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_frameproc.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/libmmcamera_hdr_lib.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_hdr_lib.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/libmmcamera_image_stab.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_image_stab.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/libmmcamera_interface.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_interface.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/libmmcamera_interface2.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_interface2.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/libmmcamera_plugin.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_plugin.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/libmmcamera_statsproc31.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_statsproc31.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/libmmcamera_wavelet_lib.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_wavelet_lib.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/liboemcamera.so:$(TARGET_COPY_OUT_VENDOR)/lib/liboemcamera.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/libsensor1.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsensor1.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/libsensor_reg.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsensor_reg.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/libmmjpeg.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmjpeg.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/hw/camera.msm8960.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/camera.msm8960.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/hw/sensors.msm8960.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/sensors.msm8960.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/hw/sensors_native_hal.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/sensors_native_hal.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/hw/sensors_qcom.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/sensors_qcom.so \
+    vendor/pantech/ef52/proprietary/vendor/lib/hw/lights.msm8960.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/lights.msm8960.so \
 
 PRODUCT_COPY_FILES += \
-    vendor/pantech/ef52/proprietary/etc/firmware/cyttsp_8064_mtp.hex:system/etc/firmware/cyttsp_8064_mtp.hex \
-    vendor/pantech/ef52/proprietary/etc/firmware/cyttsp_8960_cdp.hex:system/etc/firmware/cyttsp_8960_cdp.hex \
-    vendor/pantech/ef52/proprietary/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
-    vendor/pantech/ef52/proprietary/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
-    vendor/pantech/ef52/proprietary/etc/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
-    vendor/pantech/ef52/proprietary/etc/firmware/vidc.b02:system/etc/firmware/vidc.b02 \
-    vendor/pantech/ef52/proprietary/etc/firmware/vidc.b03:system/etc/firmware/vidc.b03 \
-    vendor/pantech/ef52/proprietary/etc/firmware/libpn544_fw.so:system/etc/firmware/libpn544_fw.so \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware/cyttsp_8064_mtp.hex:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/cyttsp_8064_mtp.hex \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware/cyttsp_8960_cdp.hex:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/cyttsp_8960_cdp.hex \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware/leia_pfp_470.fw:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/leia_pfp_470.fw \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware/leia_pm4_470.fw:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/leia_pm4_470.fw \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware/vidc_1080p.fw:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/vidc_1080p.fw \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware/vidc.b02:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/vidc.b02 \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware/vidc.b03:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/vidc.b03 \
+    vendor/pantech/ef52/proprietary/vendor/etc/firmware/libpn544_fw.so:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/libpn544_fw.so \
